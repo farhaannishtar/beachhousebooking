99407b4c5fc2c7735ae5d25adbc6ef86c5e2c1c1
Nathik <nathikazd@gmail.com>
2024-06-04 15:24:12 -0400

    show access token

--- supa/front/app/login/page.tsx
+++ supa/front/app/login/page.tsx
@@ -16,10 +16,13 @@ export default function Login({
     const password = formData.get("password") as string;
     const supabase = createClient();
 
-    const { error } = await supabase.auth.signInWithPassword({
+    const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
+
+    console.log(data.session?.access_token);
+    
 
     if (error) {
       return redirect("/login?message=Could not authenticate user");
