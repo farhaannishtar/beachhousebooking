98be476f881fc530e10ff142d8560897e1adcc78
Nathik <nathikazd@gmail.com>
2024-06-05 11:34:17 -0400

    notes create working

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,30 +1,38 @@
 import {onRequest} from "firebase-functions/v2/https";
+import * as cors from 'cors';
 import * as logger from "firebase-functions/logger";
 import { JwtPayload, query, verifyToken } from "./helper";
 
-
+const corsHandler = cors({origin: true});
 
 
 export const authenticate = onRequest((request, response) => {
+  corsHandler(request, response, () => {
     const header = request.headers.authorization;
     if (!header) {
       response.status(401).send('No token provided');
       return;
     }
-  
+
     const bearer = header.split(' ');
     const token = bearer[1]; // Assuming the Authorization header contains "Bearer [token]"
-  
+
     const verifiedToken = verifyToken(token);
-  
+
     if (verifiedToken == "Invalid token") { // Check if the returned value is the error message
       response.status(403).send('Invalid token')
-        return;
+      return;
     }
     const payload = (verifiedToken as JwtPayload)
-    query('INSERT INTO notes(text) VALUES($1)', [payload.email])
-  
+    // const { content } = request.body;
+    logger.info("😈 ")
+    let body = JSON.parse(request.body)
+    logger.info(body)
+    logger.info(body["content"])
+    query('INSERT INTO notes(text, email) VALUES($1, $2)', [body["content"], payload.email]);
+
     // Proceed with the verified JWT
     logger.info("Verified JWT:", { verifiedToken });
     response.send(`Hello from Firebase! User: ${payload.email}`);
   });
+});
--- supa/front/app/notes/page.tsx
+++ supa/front/app/notes/page.tsx
@@ -1,8 +1,70 @@
 import { createClient } from '@/utils/supabase/server';
+import { SubmitButton } from '../login/submit-button';
 
 export default async function Notes() {
   const supabase = createClient();
   const { data: notes } = await supabase.from("notes").select();
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+  
+  const createNote = async (formData: FormData) => {
+  // const testFunction = (event: React.FormEvent) => {
+    "use server";
+    console.log('Test Function');
+    
+    const url = 'https://authenticate-snss73hxzq-uc.a.run.app'; // Your Firebase function URL
+    const supabase = createClient();
+    let sesh = await supabase.auth.getSession()
+
+     //   console.log("session ", sesh.data.session?.access_token);
+    let token = sesh.data.session?.access_token;
+    const content = formData.get("content") as string;
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          'Authorization': `Bearer ${token}` // Replace YOUR_TOKEN_HERE with your actual token
+        },
+        body: JSON.stringify({
+          content: content
+        })
+      });
+      const data = await response.json(); // Assuming the response is JSON
+      console.log('Response from Firebase function:', data);
+      // You might want to handle the data further or redirect the user based on the response
+    } catch (error) {
+      console.error('Error calling Firebase function:', error);
+    }
+  }
+
+  return (
+    <div>
+      <ul>
+        {notes?.map(note => (
+          <li key={note.id}>
+            <strong>ID:</strong> {note.id} <br />
+            <strong>Text:</strong> {note.text}
+          </li>
+        ))}
+      </ul>
+      <form>
+      <label className="text-md" htmlFor="content">
+          Content
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="content"
+          placeholder="Enter note text here"
+          required
+        />
+      <SubmitButton
+        formAction={createNote}
+        className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+        pendingText="Creating Note..."
+      >
+        Creat Note
+      </SubmitButton>
+      </form>
+    </div>
+  );
 }
