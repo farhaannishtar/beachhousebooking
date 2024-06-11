874bc28fc4a8327d66df03f09cbfd700cff55705
nathikazad <nathikazad@gmail.com>
2024-06-11 14:16:26 -0400

    basic wiring

--- components/ListBooking.tsx
+++ components/ListBooking.tsx
@@ -42,51 +42,41 @@ export default function ListBooking({bookingsFromParent}: BookingProps) {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#617A8A" className="h-5 w-5 absolute z-50 left-3 pointer-events-none">
             <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clipRule="evenodd" />
           </svg>
-          <input type="search" className="relative flex-auto w-full px-10 py-[0.25rem] placeholder:text-placeHolderText bg-inputBoxbg text-neutral-700 outline-none" placeholder="Search" aria-label="Search" />
+          <input type="search" className="relative flex-auto w-full px-10 py-[0.25rem] placeholder:text-placeHolderText bg-inputBoxbg text-neutral-700 outline-none" placeholder="Search" aria-label="Search"
+             name="searchText"
+            value={state.searchText || undefined}
+            onChange={handleChangeSearch}
+            />
           {/* <FilterButtonAndModal /> */}
         </div>
       </div>
       <p className="pl-1 mt-6 text-neutral-900 text-lg font-semibold leading-6">
         Today
       </p>
-      <div className="flex mt-3 w-full justify-between">
-        <div className="pl-3">
-          <p>
-            <span className="text-neutral-900 text-base font-medium leading-6">Srinivasan</span> <span className="text-slate-500 text-sm font-normal leading-5">Inquiry</span>
-          </p>
-          <section>
-            <p className="text-slate-500 text-sm font-normal leading-5">3 days, 200 pax</p>
-            <p className="text-slate-500 text-sm font-normal leading-5">Bluehouse, Le Chalet, Glasshouse</p>
-            <p className="text-slate-500 text-sm font-normal leading-5">Referral: Google</p>
-          </section>
-        </div>
-        <div className="w-[84px] flex items-center">
-          <div className="w-[84px] h-9 px-5 bg-gray-100 rounded-[19px] justify-center items-center inline-flex items-center">
-            <div className="w-11 left-[20px] top-[6px] text-center text-sky-500 text-base font-medium leading-normal">
-              Event
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex mt-3 w-full justify-between">
-        <div className="pl-3">
-          <p>
-            <span className="text-neutral-900 text-base font-medium leading-6">Nithika Suresh</span> <span className="text-slate-500 text-sm font-normal leading-5">Quotation</span>
-          </p>
-          <section>
-            <p className="text-slate-500 text-sm font-normal leading-5">2 days, 8 pax</p>
-            <p className="text-slate-500 text-sm font-normal leading-5">Villa Armati</p>
-            <p className="text-slate-500 text-sm font-normal leading-5">Rs 25,0000</p>
-          </section>
-        </div>
-        <div className="w-[84px] flex items-center">
-          <div className="w-[84px] h-9 px-5 bg-gray-100 rounded-[19px] justify-center items-center inline-flex items-center">
-            <div className="w-11 left-[20px] top-[6px] text-center text-sky-500 text-base font-medium leading-normal">
-              Stay
-            </div>
-          </div>
-        </div>
-      </div>
+
+
+      {state.bookings?.map((booking) => (
+                <div className="flex mt-3 w-full justify-between">
+                    <div className="pl-3">
+                    <p>
+                        <span className="text-neutral-900 text-base font-medium leading-6">{booking.client.name}</span> <span className="text-slate-500 text-sm font-normal leading-5">Inquiry</span>
+                    </p>
+                    <section>
+                        <p className="text-slate-500 text-sm font-normal leading-5">3 days, 200 pax</p>
+                        <p className="text-slate-500 text-sm font-normal leading-5">Bluehouse, Le Chalet, Glasshouse</p>
+                        <p className="text-slate-500 text-sm font-normal leading-5">Referral: Google</p>
+                    </section>
+                    </div>
+                    <div className="w-[84px] flex items-center">
+                    <div className="w-[84px] h-9 px-5 bg-gray-100 rounded-[19px] justify-center items-center inline-flex items-center">
+                        <div className="w-11 left-[20px] top-[6px] text-center text-sky-500 text-base font-medium leading-normal">
+                        Event
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            ))}
+
     </div>
     );
 };
@@ -97,9 +87,7 @@ export default function ListBooking({bookingsFromParent}: BookingProps) {
         //     <input
         //         type="text"
         //         placeholder="Search"
-        //         name="searchText"
-        //         value={state.searchText || undefined}
-        //         onChange={handleChangeSearch}
+
         //     />
         //     <ul>
         //         {state.bookings?.map((booking) => (
