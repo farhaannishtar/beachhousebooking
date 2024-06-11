ea2630c1e8a92bc7c2fb19317227b7a77dfb35d4
Farhaan Nishtar <farhaannishtar@gmail.com>
2024-06-11 14:58:36 -0400

    route user to create booking from list view

--- app/protected/booking/list/page.tsx
+++ app/protected/booking/list/page.tsx
@@ -8,21 +8,20 @@ export default async function Booking() {
   const supabase = createClient();
   let { data: bookingsData } = await supabase.from("bookings").select()
 
+  let bookings: BookingDB[] = []
 
-          let bookings: BookingDB[] = []
-    
-        bookingsData?.forEach((booking) => {
-          const lastIndex = booking.json.length - 1
-          const lastBooking = booking.json[lastIndex]
-          bookings.push({
-            ...lastBooking,
-            bookingId: booking.id,
-          })
-        })
+  bookingsData?.forEach((booking) => {
+    const lastIndex = booking.json.length - 1
+    const lastBooking = booking.json[lastIndex]
+    bookings.push({
+      ...lastBooking,
+      bookingId: booking.id,
+    })
+  })
 
-  
+
   return (
-    <div className='min-h-screen flex items-start justify-center mt-4 w-full'> 
+    <div className='min-h-screen flex items-start justify-center mt-4 w-full'>
       <BookingsListComponent bookingsFromParent={bookings} />
     </div>
   );
