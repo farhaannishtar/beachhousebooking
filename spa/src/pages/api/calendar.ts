77b108c768132c998e61146ca584ef160702e13f
nathikazad <nathikazad@gmail.com>
2024-07-24 22:00:59 -0700

    made arraay

--- spa/src/pages/api/calendar.ts
+++ spa/src/pages/api/calendar.ts
@@ -16,19 +16,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 }
 
 const handleGet = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { property, month, year } = req.query;
-  let propertyInternal = convertStringToProperty(property as string)
+  const { properties, month, year } = req.query;
+  let propertiesInternal = (properties as string).split(',').map((property) => convertStringToProperty(property));
   type Month = "june" | "july" | "august" | "september" | "october" | "november" | "december" | "january" | "february" | "march" | "april" | "may";
   const monthConvert: { [key in Month]: number } = { "june": 6, "july": 7, "august": 8, "september": 9, "october": 10, "november": 11, "december": 12, "january": 1, "february": 2, "march": 3, "april": 4, "may": 5 }
   let monthNumber = monthConvert[(month as string).toLocaleLowerCase() as Month];
   let monthString = monthNumber < 10 ? `0${monthNumber}` : monthNumber;
-  let events = await listEvents(propertyInternal, `${year}-${monthString}-01T00:00:00Z`, `${year}-${monthString}-31T23:59:59Z`)
-  let json = events.map((event) => {
-    return {
-      start: event.start?.dateTime,
-      end: event.end?.dateTime
-    }
-  })
+  let json: any = [];
+  for (let property of propertiesInternal) {
+    let events = await listEvents(property, `${year}-${monthString}-01T00:00:00Z`, `${year}-${monthString}-31T23:59:59Z`)
+    json.push(events.map((event) => {
+      return {
+        start: event.start?.dateTime,
+        end: event.end?.dateTime
+      }
+    }))
+  }
+  
   res.status(200).json(json);
 };
 
