dde079dba909b8e712d615b0cf4e8b531b8061a9
YassineBouassida <45517360+YassineBouassida@users.noreply.github.com>
2024-09-07 20:13:41 +0100

    add caution to client page

--- spa/src/components/UserBookingDetails.tsx
+++ spa/src/components/UserBookingDetails.tsx
@@ -243,53 +243,108 @@ export default function BookingDetailsComponent({ bookingId }: BookingDetailsPro
                         </div>
                         <div className='flex flex-col mt-6 gap-4'>
                             {/* Name  */}
-                            <div className='w-full mb-2'>
-                                <label className='title'> {formState.form.client.name}</label>
+                            <div className="w-full mb-2 flex items-center justify-between">
+                                <label className="title"> {formState.form.client.name}</label>
                             </div>
                             {/* Phone  */}
-                            <div className='w-full flex gap-3 items-center'>
-                                <label className='label_text'>{formState.form.client.phone}</label>
+                            <div className="w-full flex gap-3 items-center">
+                                <a
+                                    href={`https://api.whatsapp.com/send?phone=${formState.form.client.phone}`}
+                                    target="_blank"
+                                >
+                                    <svg
+                                        height="17"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 448 512"
+                                    >
+                                        <path fill="#167F3C" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
+                                    </svg>
+                                </a>
+                                <label className="label_text">
+                                    {formState.form.client.phone}
+                                </label>
 
                             </div>
                             {/* Dates  */}
-                            <div className='flex flex-col  w-full gap-2'>
-                                <label className='label_text !font-semibold'>Dates</label>
-                                <div className='flex flex-col gap-1  pl-4'>
-                                    {formState.form.startDateTime && <label className='label_text '> {format(new Date(`${formState.form.startDateTime || ''}`), "iii LLL d, hh:mmaa")} - </label>}
+                            <div className="flex flex-col  w-full gap-2">
+                                <label className="label_text !font-semibold">Dates</label>
+                                <div className="flex gap-1  ">
+                                    <span className="material-symbols-outlined text-lg text-[#BEBEBE]">
+                                        calendar_month
+                                    </span>
+                                    {formState.form.startDateTime && (
+                                        <label className="label_text ">
+                                            {" "}
+                                            {format(
+                                                new Date(`${formState.form.startDateTime || ""}`),
+                                                "EEE LLL d, hh:mmaa"
+                                            )}{" "}
+                                            -{" "}
+                                        </label>
+                                    )}
 
-                                    {formState.form.endDateTime && <label className='label_text '>{format(new Date(`${formState.form.endDateTime || ''}`), "iii LLL d, hh:mmaa")}  </label>}
+                                    {formState.form.endDateTime && (
+                                        <label className="label_text ">
+                                            {format(
+                                                new Date(`${formState.form.endDateTime || ""}`),
+                                                "EEE LLL d, hh:mmaa"
+                                            )}{" "}
+                                        </label>
+                                    )}
                                 </div>
-
                             </div>
                             {/* Type of booking */}
-                            <div className='flex  flex-col'>
-                                <label className='label_text'><span className='!font-semibold'>Booking Type: </span> {formState.form.bookingType}</label>
-
+                            <div className="flex  flex-col">
+                                <label className="label_text">
+                                    <span className="!font-semibold">Booking Type: </span>{" "}
+                                    {formState.form.bookingType}
+                                </label>
                             </div>
                             {/* Numbers of events */}
-                            {formState.form.bookingType === "Event" &&
-                                <div className='flex  flex-col'>
-                                    <label className='label_text'><span className='!font-semibold'>Number of Events: </span> {formState.form.numberOfEvents}</label>
+                            {formState.form.bookingType === "Event" && (
+                                <div className="flex  flex-col">
+                                    <label className="label_text">
+                                        <span className="!font-semibold">Number of Events: </span>
 
+                                    </label>
+                                    <label className="label_text flex items-center gap-2"> <span className="material-symbols-filled text-lg text-[#BEBEBE]">
+                                        event
+                                    </span>{formState.form.numberOfEvents}</label>
                                 </div>
-                            }
+                            )}
 
                             {/* Numbers of Guests */}
-                            <div className='flex  flex-col'>
-                                <label className='label_text'><span className='!font-semibold'>Number of Guests: </span> {formState.form.numberOfGuests}</label>
+                            <div className="flex  flex-col">
+                                <label className="label_text">
+                                    <span className="!font-semibold">Number of Guests: </span>
+                                    <label className="label_text flex items-center gap-2"> <span className="material-symbols-filled text-lg text-[#BEBEBE]">
+                                        group
+                                    </span>{formState.form.numberOfGuests}</label>
+
+                                </label>
                             </div>
                             {/* Notes  */}
-                            {formState.form.notes && <div className='flex-col gap-2 flex'>
-                                <label className='label_text !font-semibold'>Notes:</label>
-                                <label className='label_text pl-4'> {formState.form.notes}</label>
-                            </div>}
-                            {/* Properties  */}
-                            {formState.form.properties && <div className='flex-col gap-2 flex'>
-                                <label className='label_text !font-semibold'>Properties: </label>
-                                <div className='flex pl-4'>
-                                    <label className='label_text'>{formState.form.properties.join(', ')}</label>
+                            {formState.form.notes && (
+                                <div className="flex-col gap-2 flex">
+                                    <label className="label_text !font-semibold">Notes:</label>
+                                    <label className="label_text pl-4">
+                                        {" "}
+                                        {formState.form.notes}
+                                    </label>
                                 </div>
-                            </div>}
+                            )}
+                            {/* Properties  */}
+                            {formState.form.properties?.length > 0 && (
+                                <div className="flex-col gap-2 flex">
+                                    <label className="label_text">
+                                        <span className="!font-semibold">Properties: </span>
+                                        <label className="label_text flex items-center gap-2"> <span className="material-symbols-filled text-lg text-[#BEBEBE]">
+                                            home
+                                        </span>{formState.form.properties.join(", ")}</label>
+
+                                    </label>
+                                </div>
+                            )}
                             {/* Status  */}
                             <div className='flex-col gap-3'>
                                 <label className='label_text !font-semibold'>Status: </label>
@@ -321,26 +376,26 @@ export default function BookingDetailsComponent({ bookingId }: BookingDetailsPro
                                             </p>
                                             {formState.form.events.map((event, index) => {
                                                 return (
-                                                  !event.markForDeletion && (
-                                                    <div
-                                                      key={index}
-                                                      className="flex items-center justify-between rounded-xl bg-typo_light-100 p-4 cursor-pointer "
-                                                    >
-                                                      <div className="flex flex-col gap-2">
-                                                        <label className="label_text p-0">{` ${event.eventName}  (${event.numberOfGuests}) (₹${event.finalCost.toLocaleString("en-IN")} )`}</label>
-                                                        {event.startDateTime && (
-                                                          <label className="label_text p-0">{`${format(new Date(`${event.startDateTime || ""}`), "iii LLL d, hh:mmaa")} `}</label>
-                                                        )}
-                                                        {event.endDateTime && (
-                                                          <label className="label_text p-0">{`${format(new Date(`${event.endDateTime || ""}`), "iii LLL d, hh:mmaa")} `}</label>
-                                                        )}
-                                                        <label className="label_text p-0">{`${event.properties.toString()}`}</label>
-                                                      </div>
-                                                      <span className="material-symbols-outlined ">
-                                                        chevron_right
-                                                      </span>
-                                                    </div>
-                                                  )
+                                                    !event.markForDeletion && (
+                                                        <div
+                                                            key={index}
+                                                            className="flex items-center justify-between rounded-xl bg-typo_light-100 p-4 cursor-pointer "
+                                                        >
+                                                            <div className="flex flex-col gap-2">
+                                                                <label className="label_text p-0">{` ${event.eventName}  (${event.numberOfGuests}) (₹${event.finalCost.toLocaleString("en-IN")} )`}</label>
+                                                                {event.startDateTime && (
+                                                                    <label className="label_text p-0">{`${format(new Date(`${event.startDateTime || ""}`), "iii LLL d, hh:mmaa")} `}</label>
+                                                                )}
+                                                                {event.endDateTime && (
+                                                                    <label className="label_text p-0">{`${format(new Date(`${event.endDateTime || ""}`), "iii LLL d, hh:mmaa")} `}</label>
+                                                                )}
+                                                                <label className="label_text p-0">{`${event.properties.toString()}`}</label>
+                                                            </div>
+                                                            <span className="material-symbols-outlined ">
+                                                                chevron_right
+                                                            </span>
+                                                        </div>
+                                                    )
                                                 );
                                             })}
 
@@ -453,6 +508,17 @@ export default function BookingDetailsComponent({ bookingId }: BookingDetailsPro
                                         <a href="upi://pay?pa=6374473550@pz&pn=Bluehouse&mc=0000&tid=1<client view id>&tt=10&am=<outstanding>&cu=INR&url=https://bluehouseecr.com" target='_blank'>
                                             <svg className="main-header__logo-image w-16" fill="#A1A1A1" role="presentation" viewBox="0 0 435.97 173.13" xmlns="http://www.w3.org/2000/svg"><path d="M206.2,84.58v50.75H190.1V10h42.7a38.61,38.61,0,0,1,27.65,10.85A34.88,34.88,0,0,1,272,47.3a34.72,34.72,0,0,1-11.55,26.6q-11.2,10.68-27.65,10.67H206.2Zm0-59.15V69.18h27a21.28,21.28,0,0,0,15.93-6.48,21.36,21.36,0,0,0,0-30.63,21,21,0,0,0-15.93-6.65h-27Z" fill="#5f6368" /><path d="M309.1,46.78q17.85,0,28.18,9.54T347.6,82.48v52.85H332.2v-11.9h-.7q-10,14.7-26.6,14.7-14.17,0-23.71-8.4a26.82,26.82,0,0,1-9.54-21q0-13.31,10.06-21.17t26.86-7.88q14.34,0,23.62,5.25V81.25A18.33,18.33,0,0,0,325.54,67,22.8,22.8,0,0,0,310,61.13q-13.49,0-21.35,11.38l-14.18-8.93Q286.17,46.78,309.1,46.78Zm-20.83,62.3a12.86,12.86,0,0,0,5.34,10.5,19.64,19.64,0,0,0,12.51,4.2,25.67,25.67,0,0,0,18.11-7.52q8-7.53,8-17.67-7.53-6-21-6-9.81,0-16.36,4.73C290.46,100.52,288.27,104.41,288.27,109.08Z" fill="#5f6368" /><path d="M436,49.58,382.24,173.13H365.62l19.95-43.23L350.22,49.58h17.5l25.55,61.6h.35l24.85-61.6Z" fill="#5f6368" /><path d="M141.14,73.64A85.79,85.79,0,0,0,139.9,59H72V86.73h38.89a33.33,33.33,0,0,1-14.38,21.88v18h23.21C133.31,114.08,141.14,95.55,141.14,73.64Z" fill="#4285f4" /><path d="M72,144c19.43,0,35.79-6.38,47.72-17.38l-23.21-18C90.05,113,81.73,115.5,72,115.5c-18.78,0-34.72-12.66-40.42-29.72H7.67v18.55A72,72,0,0,0,72,144Z" fill="#34a853" /><path d="M31.58,85.78a43.14,43.14,0,0,1,0-27.56V39.67H7.67a72,72,0,0,0,0,64.66Z" fill="#fbbc04" /><path d="M72,28.5A39.09,39.09,0,0,1,99.62,39.3h0l20.55-20.55A69.18,69.18,0,0,0,72,0,72,72,0,0,0,7.67,39.67L31.58,58.22C37.28,41.16,53.22,28.5,72,28.5Z" fill="#ea4335" /></svg>
                                         </a>
+                                    </div>
+                                </div>
+                                {/* Caution  */}
+                                <div className='flex flex-col gap-2'>
+                                    <label className='label_text !font-semibold'>Caution:</label>
+                                    <div className='flex items-center gap-3 bg-warning/20 rounded-xl p-4'>
+                                        <span className="material-symbols-outlined text-xl text-warning">
+                                            warning
+                                        </span>
+                                        <p >No refund no cancellation
+                                            50 % advance</p>
                                     </div>
                                 </div>
                             </div>
